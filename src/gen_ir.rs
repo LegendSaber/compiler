57a8b504e6c5b383719691fd4f7946c81ea84cc2
1900 <747671402@qq.com>
2024-07-05 16:28:32 +0800

    表达式翻译

--- src/gen_ir.rs
+++ src/gen_ir.rs
@@ -1,8 +1,10 @@
 use std::sync::Mutex;
 use lazy_static::lazy_static;
-use crate::common::Operator::{OpAs, OpEntry, OpExit, OpGet, OpLea, OpRet, OpRetv, OpSet};
-use crate::common::SemError::{AssignTypeErr, ExprIsBase, ExprNotLeftVal, ReturnErr};
-use crate::common::Tag::KwVoid;
+use crate::common::Operator::{OpAdd, OpAnd, OpAs, OpDiv, OpEntry, OpEqu, OpExit, OpGe, OpGet, OpGt, OpLe, OpLea, OpLt, OpMod, OpMul, OpNe, OpNeg, OpNot, OpOr, OpRet, OpRetv, OpSet, OpSub};
+use crate::common::SemError::{ArrTypeErr, AssignTypeErr, ExprIsBase, ExprIsVoid, ExprNotBase, ExprNotLeftVal, ReturnErr};
+use crate::common::Tag;
+use crate::common::Tag::{OR, AND, EQU, NEQU, ADD, SUB, GT, GE, LT, LE, MUL, DIV, MOD, LEA, INC, DEC, NOT};
+use crate::common::Tag::{ASSIGN, KwInt, KwVoid};
 use crate::intercode::InterInst;
 use crate::symbol::{Fun, sem_error, Var};
 use crate::symtab::SymTab;
@@ -21,6 +23,7 @@ pub(crate) struct GenIR {
 }
 
 impl GenIR {
+    // 全局函数
 
     // 获取唯一名字的标签
     pub(crate) fn gen_lb() -> String {
@@ -32,10 +35,7 @@ impl GenIR {
 
     // 检查类型是否可以转换
     pub(crate) fn type_check(&self, lval: Option<Box<Var>>, rval: Option<Box<Var>>) -> bool {
-        if let None = lval {
-            return false;
-        }
-        if let None = rval {
+        if lval.is_none() || rval.is_none() {
             return false;
         }
         let lval = lval.unwrap();
@@ -52,6 +52,8 @@ impl GenIR {
 }
 
 impl GenIR {
+    // 产生特殊语句
+
     // 产生函数入口语句
     pub(crate) fn gen_fun_head(&mut self, mut function: Box<Fun>) {
         function.enter_scope();      // 进入函数作用域
@@ -88,16 +90,168 @@ impl GenIR {
                     if ret.is_ref() {
                         // 处理ret是*p的情况
                         let r = self.gen_assign(ret.clone());
-                        let inst = Box::new(InterInst::new_jump(OpRetv, return_point, r, None));
+                        let inst = Box::new(InterInst::new_jump(OpRetv, return_point, Some(r), None));
                         self.sym_tab.add_inst(inst);
                     }
                 }
             }
         }
     }
+}
+
+impl GenIR {
+    /* 产生符号和语句 */
+
+    // 双目运算语句
+    pub(crate) fn gen_two_op(&mut self, lval: Option<Box<Var>>, opt: Tag, rval: Option<Box<Var>>) -> Option<Box<Var>> {
+        if lval.is_none() || rval.is_none() {
+            return None;
+        }
+        let mut lval = lval.unwrap();
+        let mut rval = rval.unwrap();
+
+        if lval.is_void() || rval.is_void() {
+            sem_error(ExprIsVoid as usize, "");     // void函数返回值不能出现在表达式中
+            return None;
+        }
+
+        if !lval.is_base() || !rval.is_base() {
+            sem_error(ExprNotBase as usize, "");    // 不是基本类型
+            return Some(lval);
+        }
+
+        // 赋值单独处理
+        if opt == ASSIGN {
+            return Some(self.gen_assign_stmt(lval.clone(), rval.clone()));
+        }
+
+        // 先处理(*p)变量
+        if lval.is_ref() {
+            lval = self.gen_assign(lval.clone()).unwrap();
+        }
+
+        if rval.is_ref() {
+            rval = self.gen_assign(rval.clone()).unwrap();
+        }
+
+        let ret = match opt {
+            // 或
+            OR  => self.gen_or(lval.clone(), rval.clone()),
+            // 与
+            AND => self.gen_and(lval.clone(), rval.clone()),
+            // 等于
+            EQU => self.gen_equ(lval.clone(), rval.clone()),
+            // 不等于
+            NEQU => self.gen_nequ(lval.clone(), rval.clone()),
+            // 加
+            ADD => self.gen_add(lval.clone(), rval.clone()),
+            // 减
+            SUB => self.gen_sub(lval.clone(), rval.clone()),
+            // 大于
+            GT => self.gen_gt(lval.clone(), rval.clone()),
+            // 大于等于
+            GE => self.gen_gt(lval.clone(), rval.clone()),
+            // 小于
+            LT => self.gen_lt(lval.clone(), rval.clone()),
+            // 小于等于
+            LE => self.gen_le(lval.clone(), rval.clone()),
+            // 乘
+            MUL => self.gen_mul(lval.clone(), rval.clone()),
+            // 除
+            DIV => self.gen_div(lval.clone(), rval.clone()),
+            // 取模
+            MOD => self.gen_mod(lval.clone(), rval.clone()),
+            // 默认返回左值
+            _ => lval,
+        };
+
+        Some(ret)
+    }
+
+    // 左单目运算语句
+    pub(crate) fn get_one_op_left(&mut self, opt: Tag, val: Option<Box<Var>>) -> Option<Box<Var>> {
+        if val.is_none() {
+            return None;
+        }
+
+        let val = val.clone().unwrap();
+        if val.is_void() {
+            sem_error(ExprIsVoid as usize, "");
+            return None;
+        }
+
+        if val.is_ref() {
+            return Some(self.gen_assign(val.clone()));
+        }
+
+        let ret = match opt {
+            LEA => self.gen_lea(val.clone()),
+            MUL => self.gen_ptr(val.clone()),
+            INC => self.gen_incl(val.clone()),
+            DEC => self.gen_decl(val.clone()),
+            NOT => self.gen_not(val.clone()),
+            SUB => self.gen_minus(val.clone()),
+            _ => val.clone(),
+        };
+
+        Some(ret)
+    }
+
+    // 右单目运算语句
+    pub(crate) fn get_one_op_right(&mut self, opt: Tag, val: Option<Box<Var>>) -> Option<Box<Var>> {
+        if val.is_none() {
+            return None;
+        }
+
+        let val = val.unwrap();
+
+        if val.is_void() {
+            sem_error(ExprIsVoid as usize, "");
+            return None;
+        }
+
+        if !val.get_left() {
+            sem_error(ExprNotLeftVal as usize, "");
+            return Some(val.clone());
+        }
+
+        let mut ret = val.clone();
+        if opt == INC {
+            ret = self.gen_incr(val.clone());
+        } else if opt == DEC {
+            ret = self.gen_decr(val.clone());
+        }
+
+        Some(ret)
+    }
+
+
+    // 数组索引语句
+    pub(crate) fn gen_array(&mut self, array: Option<Box<Var>>, index: Option<Box<Var>>) -> Option<Box<Var>> {
+        if array.is_none() || index.is_none() {
+            return None;
+        }
+
+        let array = array.unwrap();
+        let index = index.unwrap();
+
+        if array.is_base() || !index.is_base() {
+            sem_error(ArrTypeErr as usize, "");
+            return Some(index);
+        }
+
+        let v = self.gen_add(array, index);
+        let ret = self.gen_ptr(v);
+
+        Some(ret)
+    }
+}
+
+impl GenIR {
+    /* 双目运算 */
 
     // 拷贝赋值语句，处理*p的情况
-    pub(crate) fn gen_assign(&mut self, val: Box<Var>) -> Option<Box<Var>> {
+    pub(crate) fn gen_assign(&mut self, val: Box<Var>) -> Box<Var> {
         let tmp = Box::new(Var::new_copy_temp(self.sym_tab.get_scope_path(), val.clone()));   // 拷贝变量信息
 
         let t = tmp.clone();
@@ -111,7 +265,7 @@ impl GenIR {
 
         self.sym_tab.add_inst(inst);
 
-        Some(tmp)
+        tmp
     }
 
     // 赋值语句
@@ -137,7 +291,7 @@ impl GenIR {
                 return lval;
             } else {
                 // 中间代码*(lval->ptr)=*(rval->ptr),先处理右值
-                r  = self.gen_assign(rval.clone());
+                r  = Some(self.gen_assign(rval.clone()));
             }
         }
 
@@ -155,9 +309,195 @@ impl GenIR {
         lval
     }
 
-    /*
-	    指针取值语句
-    */
+    // 或运算语句
+    pub(crate) fn gen_or(&mut self, lval: Box<Var>, rval: Box<Var>) -> Box<Var> {
+        let tmp = Box::new(Var::new_temp(self.sym_tab.get_scope_path(), KwInt, false));
+
+        self.sym_tab.add_var(tmp.clone());
+        let inst = Box::new(InterInst::new_common(OpOr, tmp.clone(), Some(lval.clone()), Some(rval.clone())));
+        self.sym_tab.add_inst(inst);
+
+        tmp
+    }
+
+    // 与运算符
+    pub(crate) fn gen_and(&mut self, lval: Box<Var>, rval: Box<Var>) -> Box<Var> {
+        let tmp = Box::new(Var::new_temp(self.sym_tab.get_scope_path(), KwInt, false));
+
+        self.sym_tab.add_var(tmp.clone());
+        let inst = Box::new(InterInst::new_common(OpAnd, tmp.clone(), Some(lval.clone()), Some(rval.clone())));
+        self.sym_tab.add_inst(inst);
+
+        tmp
+    }
+
+    // 大于运算符
+    pub(crate) fn gen_gt(&mut self, lval: Box<Var>, rval: Box<Var>) -> Box<Var> {
+        let tmp = Box::new(Var::new_temp(self.sym_tab.get_scope_path(), KwInt, false));
+
+        self.sym_tab.add_var(tmp.clone());
+        let inst = Box::new(InterInst::new_common(OpGt, tmp.clone(), Some(lval.clone()), Some(rval.clone())));
+        self.sym_tab.add_inst(inst);
+
+        tmp
+    }
+
+    // 大于等于运算符
+    pub(crate) fn gen_ge(&mut self, lval: Box<Var>, rval: Box<Var>) -> Box<Var> {
+        let tmp = Box::new(Var::new_temp(self.sym_tab.get_scope_path(), KwInt, false));
+
+        self.sym_tab.add_var(tmp.clone());
+        let inst = Box::new(InterInst::new_common(OpGe, tmp.clone(), Some(lval.clone()), Some(rval.clone())));
+        self.sym_tab.add_inst(inst);
+
+        tmp
+    }
+
+    // 小于运算符
+    pub(crate) fn gen_lt(&mut self, lval: Box<Var>, rval: Box<Var>) -> Box<Var> {
+        let tmp = Box::new(Var::new_temp(self.sym_tab.get_scope_path(), KwInt, false));
+
+        self.sym_tab.add_var(tmp.clone());
+        let inst = Box::new(InterInst::new_common(OpLt, tmp.clone(), Some(lval.clone()), Some(rval.clone())));
+        self.sym_tab.add_inst(inst);
+
+        tmp
+    }
+
+    // 小于等于运算符
+    pub(crate) fn gen_le(&mut self, lval: Box<Var>, rval: Box<Var>) -> Box<Var> {
+        let tmp = Box::new(Var::new_temp(self.sym_tab.get_scope_path(), KwInt, false));
+
+        self.sym_tab.add_var(tmp.clone());
+        let inst = Box::new(InterInst::new_common(OpLe, tmp.clone(), Some(lval.clone()), Some(rval.clone())));
+        self.sym_tab.add_inst(inst);
+
+        tmp
+    }
+
+    // 等于运算符
+    pub(crate) fn gen_equ(&mut self, lval: Box<Var>, rval: Box<Var>) -> Box<Var> {
+        let tmp = Box::new(Var::new_temp(self.sym_tab.get_scope_path(), KwInt, false));
+
+        self.sym_tab.add_var(tmp.clone());
+        let inst = Box::new(InterInst::new_common(OpEqu, tmp.clone(), Some(lval.clone()), Some(rval.clone())));
+        self.sym_tab.add_inst(inst);
+
+        tmp
+    }
+
+    // 不等于运算符
+    pub(crate) fn gen_nequ(&mut self, lval: Box<Var>, rval: Box<Var>) -> Box<Var> {
+        let tmp = Box::new(Var::new_temp(self.sym_tab.get_scope_path(), KwInt, false));
+
+        self.sym_tab.add_var(tmp.clone());
+        let inst = Box::new(InterInst::new_common(OpNe, tmp.clone(), Some(lval.clone()), Some(rval.clone())));
+        self.sym_tab.add_inst(inst);
+
+        tmp
+    }
+
+    // 加法运算符
+    pub(crate) fn gen_add(&mut self, lval: Box<Var>, rval: Box<Var>) -> Box<Var> {
+        let tmp;
+        let mut lval = lval.clone();
+        let mut rval = rval.clone();
+        // 指针和数组只能和基本类型相加
+        if (lval.get_array() || lval.get_ptr()) && rval.is_base() {
+            tmp = Box::new(Var::new_copy_temp(self.sym_tab.get_scope_path(), lval.clone()));
+            let step = Var::get_step(lval);
+            if let Some(s) = step {
+                rval = self.gen_mul(rval, s);
+            }
+        } else if rval.is_base() && (rval.get_array() || rval.get_ptr()) {
+            tmp = Box::new(Var::new_copy_temp(self.sym_tab.get_scope_path(), rval.clone()));
+            let stop = Var::get_step(rval);
+            if let Some(s) = stop {
+                lval = self.gen_mul(lval, s);
+            }
+        } else if lval.is_base() && rval.is_base() {
+            // 基本类型
+            tmp = Box::new(Var::new_temp(self.sym_tab.get_scope_path(), KwInt, false));
+        } else {
+            sem_error(ExprNotBase as usize, "");
+            tmp = lval.clone();
+        }
+
+        // 加法命令
+        self.sym_tab.add_var(tmp.clone());
+        let inst = Box::new(InterInst::new_common(OpAdd, tmp.clone(), Some(lval.clone()), Some(rval.clone())));
+        self.sym_tab.add_inst(inst);
+
+        tmp
+    }
+
+    // 减法运算符
+    pub(crate) fn gen_sub(&mut self, lval: Box<Var>, rval: Box<Var>) -> Box<Var> {
+        let tmp;
+        let mut lval = lval.clone();
+        let mut rval = rval.clone();
+
+        if !rval.is_base() {
+            sem_error(ExprNotBase as usize, "");
+            tmp = lval.clone();
+        } else if lval.get_array() || lval.get_ptr() {
+            // 指针和数组
+            tmp = Box::new(Var::new_copy_temp(self.sym_tab.get_scope_path(), lval));
+            let step = Var::get_step(lval.clone());
+            if let Some(s) = step {
+                rval = self.gen_mul(rval.clone(), s);
+            }
+        } else {
+            // 基本类型
+            tmp = Box::new(Var::new_temp(self.sym_tab.get_scope_path(), KwInt, false));
+        }
+
+        // 减法命令
+        self.sym_tab.add_var(tmp.clone());
+        let inst = Box::new(InterInst::new_common(OpSub, tmp.clone(), Some(lval.clone()), Some(rval.clone())));
+        self.sym_tab.add_inst(inst);
+
+        tmp
+    }
+
+    // 乘法运算符
+    pub(crate) fn gen_mul(&mut self, lval: Box<Var>, rval: Box<Var>) -> Box<Var> {
+        let tmp = Box::new(Var::new_temp(self.sym_tab.get_scope_path(), lval.get_type(), false));
+
+        self.sym_tab.add_var(tmp.clone());
+        let inst = Box::new(InterInst::new_common(OpMul, tmp.clone(), Some(lval.clone()), Some(rval.clone())));
+        self.sym_tab.add_inst(inst);
+
+        tmp
+    }
+
+    // 除法运算符
+    pub(crate) fn gen_div(&mut self, lval: Box<Var>, rval: Box<Var>) -> Box<Var> {
+        let tmp = Box::new(Var::new_temp(self.sym_tab.get_scope_path(), lval.get_type(), false));
+
+        self.sym_tab.add_var(tmp.clone());
+        let inst = Box::new(InterInst::new_common(OpDiv, tmp.clone(), Some(lval.clone()), Some(rval.clone())));
+        self.sym_tab.add_inst(inst);
+
+        tmp
+    }
+
+    // 模运算
+    pub(crate) fn gen_mod(&mut self, lval: Box<Var>, rval: Box<Var>) -> Box<Var> {
+        let tmp = Box::new(Var::new_temp(self.sym_tab.get_scope_path(), lval.get_type(), false));
+
+        self.sym_tab.add_var(tmp.clone());
+        let inst = Box::new(InterInst::new_common(OpMod, tmp.clone(), Some(lval.clone()), Some(rval.clone())));
+        self.sym_tab.add_inst(inst);
+
+        tmp
+    }
+}
+
+impl GenIR {
+    /* 单目运算 */
+
+    // 指针取值语句
     pub(crate) fn gen_ptr(&mut self, val: Box<Var>) -> Box<Var> {
         return if val.is_base() {
             sem_error(ExprIsBase as usize, "");
@@ -174,7 +514,7 @@ impl GenIR {
         }
     }
 
-    // 取值语句
+    // 取址语句
     pub(crate) fn get_lea(&mut self, val: Box<Var>) -> Option<Box<Var>> {
         return if !val.get_left() {
             sem_error(ExprNotLeftVal as usize, ""); // 不能取地址
@@ -194,4 +534,94 @@ impl GenIR {
             }
         }
     }
+
+    // 取反
+    pub(crate) fn get_not(&mut self, val: Box<Var>) -> Option<Box<Var>> {
+        // 生成整数
+        let tmp = Box::new(Var::new_temp(self.sym_tab.get_scope_path(), KwInt, false));
+
+        self.sym_tab.add_var(tmp.clone());
+        let inst = Box::new(InterInst::new_common(OpNot, tmp.clone(), Some(val.clone()), None));
+        self.sym_tab.add_inst(inst);
+
+        Some(tmp)
+    }
+
+    // 取负
+    pub(crate) fn get_minus(&mut self, val: Box<Var>) -> Option<Box<Var>> {
+        if !val.is_base() {
+            sem_error(ExprNotBase as usize, "");
+            return Some(val.clone());
+        }
+
+        let tmp = Box::new(Var::new_temp(self.sym_tab.get_scope_path(), KwInt, false));
+
+        self.sym_tab.add_var(tmp.clone());
+        let inst = Box::new(InterInst::new_common(OpNeg, tmp.clone(), Some(val.clone()), None));
+        self.sym_tab.add_inst(inst);
+
+        Some(tmp)
+    }
+
+    // 左自加
+    pub(crate) fn get_incl(&mut self, val: Box<Var>) -> Option<Box<Var>> {
+        if !val.get_left() {
+            sem_error(ExprNotLeftVal as usize, "");
+            return Some(val.clone());
+        }
+
+        if val.is_ref() {                                                       // ++*p
+            let t1 = self.gen_assign(val.clone());                   // t1 = *p
+            let step = Var::get_step(val);
+            if let Some(s) = step {
+                let t2 = self.gen_add(t1, s);                        // t2 = t1 + 1
+                return Some(self.gen_assign_stmt(val.clone(), t2.clone()));     // *p = t2
+            }
+        }
+
+        let inst = Box::new(InterInst::new_common(OpAdd, val.clone(), Some(val.clone()), Var::get_step(val.clone())));
+        self.sym_tab.add_inst(inst);
+
+        Some(val.clone())
+    }
+
+    // 左自减
+    pub(crate) fn gen_decl(&mut self, val: Box<Var>) -> Option<Box<Var>> {
+        if !val.get_left() {
+            sem_error(ExprNotLeftVal as usize, "");
+            return Some(val.clone());
+        }
+
+        if val.is_ref() {                                                          // ++*p
+            let t1 = self.gen_assign(val.clone());                      // t1 = *p
+            let step = Var::get_step(val);
+            if let Some(s) = step {
+                let t2 = self.gen_sub(t1, s);                           // t2 = t1 + 1
+                return Some(self.gen_assign_stmt(val.clone(), t2.clone()));        // *p = t2
+            }
+        }
+
+        Some(val.clone())
+    }
+
+    // 右自加
+    pub(crate) fn get_incr(&mut self, val: Box<Var>) -> Option<Box<Var>> {
+        let tmp = self.gen_assign(val.clone());
+
+        let inst = Box::new(InterInst::new_common(OpAdd, val.clone(), Some(val.clone()), Var::get_step(val.clone())));  // 中间代码val++
+        self.sym_tab.add_inst(inst);
+
+        Some(tmp)
+    }
+
+    // 右自减
+    pub(crate) fn gen_decr(&mut self, val: Box<Var>) -> Option<Box<Var>> {
+        let tmp = self.gen_assign(val.clone());
+
+        let inst = Box::new(InterInst::new_common(OpSub, val.clone(), Some(val.clone()), Var::get_step(val.clone())));  // 中间代码val--
+        self.sym_tab.add_inst(inst);
+
+        Some(tmp)
+    }
+
 }
